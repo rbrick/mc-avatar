08bb19f74cfbabf9043f9a89b099fc1d47b450fe
rbrick <rbrickproductions123@gmail.com>
2014-11-24 22:53:04 -0600

    Add helms & utilities method provided by @LapisBlue and @minecrell I take no credit for these.

--- mc.go
+++ mc.go
@@ -63,8 +63,35 @@ func GetSkin(userName string) (skin *Skin, err error) {
   return &Skin{Img: img, Name: userName}, nil
 }
 
+func (skin *Skin) GetHelm(size int) image.Image {
+   sk := image.NewRGBA(image.Rect(8,8,16,16))
+   hat := image.NewRGBA(getRectangle(40,8,8,8))
 
-// Size -> ratio so 1:1 is 8x8 2:1 16x16
+  for minXh := hat.Bounds().Min.X; minXh < hat.Bounds().Max.X; minXh++ {
+    for minYh := hat.Bounds().Min.Y; minYh < hat.Bounds().Max.Y; minYh++ {
+      hat.Set(minXh, minYh, skin.Img.At(minXh, minYh))
+    }
+  }
+
+   draw.Draw(sk, sk.Bounds(), skin.Img, image.Point{0,0}, draw.Src)
+
+   for minX := 0; minX < sk.Bounds().Max.X; minX++ {
+     for minY := 0; minY < sk.Bounds().Max.Y; minY++ {
+        sk.Set(minX, minY, skin.Img.At(minX, minY))
+     }
+   }
+
+  if !IsSolidColor(hat) {
+    draw.Draw(sk, sk.Bounds(), hat, hat.Bounds().Min, draw.Over)
+  }
+
+  rimg := imaging.Resize(sk, size, 0, imaging.NearestNeighbor)
+
+  return rimg
+}
+
+
+
 func (skin *Skin) GetFace(size int) image.Image {
 
   m := image.NewRGBA(image.Rect(8,8,16,16))
@@ -80,4 +107,28 @@ func (skin *Skin) GetFace(size int) image.Image {
   rimg := imaging.Resize(m, size, 0, imaging.NearestNeighbor)
 
   return rimg
+}
+
+/**
+  The following methods are from LapisBlue's project Lapitar.
+  Credits to @LapisBlue & @minecrell
+
+  I take no credit for these methods, nor do I claim they are my own. I am only
+  using these methods to make my life easier. Again I take no credit. If you would
+  like me to take this down I will.
+*/
+func IsSolidColor(img image.Image) bool {
+  base := img.At(img.Bounds().Min.X, img.Bounds().Min.Y)
+  for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+    for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+      if img.At(x, y) != base {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+func getRectangle(x,y,width, height int) image.Rectangle {
+  return image.Rect(x,y, x+width, y+height)
 }
--- server.go
+++ server.go
@@ -12,8 +12,8 @@ import (
 func main() {
     m := martini.Classic()
 
-    m.Get("/", func() {
-        fmt.Println("Hello World!\nappend /face/<playername> or /face/<size>/<playername>\nto get the face you want!")
+    m.Get("/", func() string {
+        return "Hello World!\nappend /face/<playername> or /face/<size>/<playername>\nto get the face you want!"
       })
 
     m.Get("/face/:player", func(w http.ResponseWriter,r *http.Request,params martini.Params) {
@@ -50,6 +50,41 @@ func main() {
 
           }
     })
+
+    m.Get("/helm/:player", func(w http.ResponseWriter,r *http.Request,params martini.Params) {
+      name := params["player"]
+      if name == "" {
+        fmt.Fprintln(w,"name must not be empty!")
+        //name = "steve"
+        } else if len(name) > 16 {
+          fmt.Fprintln(w,"name to long!")
+          //          name = "steve"
+          } else {
+            skin, err := GetSkin(name)
+
+            if err != nil {
+              fmt.Fprintln(w,"An internal error has occurred!")
+              return
+            }
+
+            img := skin.GetHelm(128)
+
+            if img == nil {
+              fmt.Fprintln(w,"image not found!")
+              return
+            }
+
+            w.Header().Set("Content-Type", "image/jpeg")
+
+            err1 := png.Encode(w, img)
+
+            if err1 != nil {
+              log.Fatal(err1)
+            }
+
+
+          }
+          })
 
     m.Get("/face/:size/:player", func(w http.ResponseWriter,r *http.Request,params martini.Params) {
 
@@ -92,6 +127,48 @@ func main() {
             }
           }
       })
+
+      m.Get("/helm/:size/:player", func(w http.ResponseWriter,r *http.Request,params martini.Params) {
+
+        size, err := strconv.Atoi(params["size"])
+
+        // something went wrong with the request
+        if err != nil {
+          size = 128
+        }
+
+        if size > 512 || size <= 10 {
+          size = 128
+        }
+
+        name := params["player"]
+        if name == "" {
+          fmt.Fprintln(w, "name must not be empty!")
+          } else if len(name) > 16 {
+            fmt.Fprintln(w, "name to long!")
+
+            } else {
+              skin, err := GetSkin(name)
+
+              if err != nil {
+                log.Fatal(err)
+              }
+
+              img := skin.GetHelm(size)
+
+              if img == nil {
+                fmt.Fprintln(w,"image not found!")
+              }
+
+              w.Header().Set("Content-Type", "image/jpeg")
+
+              err1 := png.Encode(w, img)
+
+              if err1 != nil {
+                log.Fatal(err1)
+              }
+            }
+            })
 
     http.ListenAndServe(":80", m)
 }
